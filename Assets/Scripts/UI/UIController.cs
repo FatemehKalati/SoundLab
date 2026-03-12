f9078ed6b68bde1f0860efa769dc986b4e8527bf
f_kalati <fatemeh.kalati@gmail.com>
2026-03-12 14:45:33 +0100

    simple title page

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -6,45 +6,31 @@ namespace SoundLab.UI
 {
     public class UIController : MonoBehaviour
     {
+        [Header("UI Panels")]
+        [SerializeField] private GameObject _titleOverlayPanel;
 
-        [Header("Panels")]
-        [SerializeField] private GameObject _UIPanel;
-        [SerializeField] private GameObject _titlePanel;
-
-        // Title Scene 
-        [Header("Title")]
+        [Header("Title Buttons")]
         [SerializeField] private Button _enterLabBtn;
         [SerializeField] private Button _titleQuitBtn;
 
-
-        [Header("UI")]
-        [SerializeField] private Button _restartBtn; // The variable for the Inspector
-        [SerializeField] private Button _uiQuitBtn;
-        [SerializeField] private Button _returnLabBtn;
-
-
-        //Lab Scene
-        // [Header("Lab")]
-        //TODO: lab button added for test here
-        // [SerializeField] private Button _backToTitleBtn;
-
         private void Start()
         {
-            if (_titlePanel && _UIPanel)
-            {
-                ShowTitle();
-            }
+            // Force the overlay to be visible when the game starts
+            if (_titleOverlayPanel) _titleOverlayPanel.SetActive(true);
+
             if (_enterLabBtn) _enterLabBtn.onClick.AddListener(OnEnterLab);
             if (_titleQuitBtn) _titleQuitBtn.onClick.AddListener(OnQuit);
-            if (_uiQuitBtn) _uiQuitBtn.onClick.AddListener(OnQuit);
-            // if (_backToTitleBtn) _backToTitleBtn.onClick.AddListener(OnBackToTitle);
-            if (_restartBtn) _restartBtn.onClick.AddListener(OnRestart);
-            if (_returnLabBtn) _returnLabBtn.onClick.AddListener(OnEnterLab);
         }
 
-        // switches scenes
-        private void OnEnterLab() => GameController.Instance.Scenes.GoToLab();
-        private void OnBackToTitle() => GameController.Instance.Scenes.GoToTitle();
+        // Hides the overlay to reveal the Lab behind it
+        private void OnEnterLab()
+        {
+            if (_titleOverlayPanel)
+            {
+                _titleOverlayPanel.SetActive(false);
+                Debug.Log("Entered Lab: Title Overlay hidden.");
+            }
+        }
 
         private void OnQuit()
         {
@@ -53,24 +39,6 @@ namespace SoundLab.UI
 #else
             Application.Quit();
 #endif
-        }
-
-
-
-        public void ShowUI()
-        {
-            _titlePanel.SetActive(false);
-            _UIPanel.SetActive(true);
-        }
-
-        public void ShowTitle()
-        {
-            _titlePanel.SetActive(true);
-            _UIPanel.SetActive(false);
-        }
-        private void OnRestart()
-        {
-            Debug.Log("UI Action: Restart triggered. (Logic not yet implemented)");
         }
     }
 }
